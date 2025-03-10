c53947129591867b50da5cabd3b5f79d866d8d2a
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-03-10 21:26:01 +0700

    Fungsi utama:  Menampilkan array berisi 5 bilangan genap. Menggunakan perulangan untuk mencetak angka satu per satu.

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/TP_MODUL2_GANESGEMIPUTRA/NumberProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class NumberProcessor
+{
+    public static void TampilkanBilanganGenap()
+    {
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
