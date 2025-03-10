db29fd4debc1e3a845517e37f91f8f993134eba5
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-03-10 21:26:52 +0700

    Fungsi utama:  Memastikan pengguna hanya memasukkan satu huruf kapital yang valid.

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/TP_MODUL2_GANESGEMIPUTRA/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Utilities
+{
+    public static char GetValidCharacter(string message)
+    {
+        char input;
+        do
+        {
+            Console.Write(message);
+            string userInput = Console.ReadLine();
+
+            if (userInput.Length == 1 && Char.IsLetter(userInput[0]))
+            {
+                input = Char.ToUpper(userInput[0]);
+                return input;
+            }
+
+            Console.WriteLine("Input tidak valid! Masukkan satu huruf kapital.");
+        } while (true);
+    }
+}
