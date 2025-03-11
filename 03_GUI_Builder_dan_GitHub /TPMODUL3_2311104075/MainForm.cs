c85d1f3f18d690a86c797d1b1ee07aec390a31de
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-03-11 09:29:28 +0700

    Menangani event ketika tombol ditekan. Mengambil input dari TextBox dan menampilkannya di Label.

---
+++ 03_GUI_Builder_dan_GitHub /TPMODUL3_2311104075/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PraktikumKPLModul3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            string nama = inputTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(nama))
+            {
+                outputLabel.Text = $"Halo {nama}!";
+            }
+            else
+            {
+                outputLabel.Text = "Masukkan nama terlebih dahulu!";
+            }
+        }
+    }
+}
