c163660dafffdff34e4b91e1169dc1ea1e6d4be6
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-03-19 01:09:36 +0700

    Add files via upload

---
+++ 05_Generics /TP_MODUL5_GANESGEMIPUTRA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Meminta input nama dari pengguna
+        Console.Write("Masukkan nama Anda: ");
+        string nama = Console.ReadLine();
+
+        // Memanggil generic method dengan nama pengguna
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(nama);
+
+        // Meminta input NIM dari pengguna
+        Console.Write("Masukkan NIM Anda: ");
+        string nim = Console.ReadLine();
+
+        // Memanggil generic class dengan NIM pengguna
+        DataGeneric<string> dataNIM = new DataGeneric<string>(nim);
+        dataNIM.PrintData();
+    }
+}
+
