5610fe9eeec4605afac5e1d7ad87bcf3c8593a64
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-04-12 15:13:33 +0700

    Program ini membaca data JSON terkait kuliah dan mahasiswa menggunakan dua kelas, yaitu KuliahMahasiswa2311104075 dan DataMahasiswa2311104075. Kedua kelas tersebut memiliki metode ReadJSON() yang digunakan untuk memuat dan mengolah data dari file JSON yang menyimpan informasi tentang jadwal kuliah dan data mahasiswa.

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TPMODUL7_KELOMPOK8/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using tpmodul7_kelompok_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KuliahMahasiswa2311104075.ReadJSON();
+        DataMahasiswa2311104075.ReadJSON();
+    }
+}
