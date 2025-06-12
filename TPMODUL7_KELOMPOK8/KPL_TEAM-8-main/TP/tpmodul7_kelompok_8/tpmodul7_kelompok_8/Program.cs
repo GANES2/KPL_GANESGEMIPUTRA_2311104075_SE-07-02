f722fd448d3103af98050d2f59aad6e76589a50a
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-06-13 02:05:16 +0700

    menambahkan jurnal modul7.pdf

---
+++ TPMODUL7_KELOMPOK8/KPL_TEAM-8-main/TP/tpmodul7_kelompok_8/tpmodul7_kelompok_8/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using tpmodul7_kelompok_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KuliahMahasiswa2311104078.ReadJSON();
+        DataMahasiswa2311104078.ReadJSON();
+    }
+}
